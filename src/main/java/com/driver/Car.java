dafe6b4198fafeabb78b312da7533ab02567315f
skyadav456 <sharadk4545@gmail.com>
2026-04-05 23:53:31 +0530

    subtask 1

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -79,12 +79,13 @@ public class Car extends Vehicle {
     }
 
     public void changeGear(int newGear){
-        this.currentGear = newGear;
-        System.out.println("changeGear method called - The gear is changed to: " + currentGear);
+        if(newGear > 0 && newGear <= gears){
+            this.currentGear = newGear;
+            System.out.println("changeGear method called - The gear is changed to: " + currentGear);
+        }
     }
 
     public void changeSpeed(int newSpeed, int newDirection){
-        move(newSpeed, newDirection);
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed +
                 ", and the direction is changed to: " + newDirection + " degrees");
     }
